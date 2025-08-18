d52e9e610d1b46d553d6a602ddf01e1a4a2e3d26
ben <benedict907@gmail.com>
2025-08-19 01:25:02 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import gspread
+from google.oauth2.service_account import Credentials
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+# 👇 allow frontend to talk to backend
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # or ["http://localhost:5173"] for Vite
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+# Google Sheets Auth
+SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
+creds = Credentials.from_service_account_file("credentials.json", scopes=SCOPES)
+client = gspread.authorize(creds)
+
+SHEET_ID = "1IDBkc4Lh8SueHu3_GMjeybm92Xwiefc7LfeQwsQd-sY"
+sheet = client.open_by_key(SHEET_ID).worksheet('August')
+
+class Expense(BaseModel):
+    date: str
+    category: str
+    description: str
+    amount: float
+
+@app.post("/add-expense")
+def add_expense(expense: Expense):
+    sheet.append_row([expense.date, expense.category, expense.description, expense.amount])
+    return {"status": "success", "data": expense}
